5e47801dd81479d8628d114dc26f4177d6eab0b7
PMBytner <pawel.m.bytner@gmail.com>
2023-09-30 10:57:06 +0200

    Dodaj pliki projektów.

---
+++ ASP_NET_ INMN3(hybryda)_PA2_Z1/Pages/NowaStrona.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP_NET__INMN3_hybryda__PA2_Z1.Pages
+{
+    public class NowaStronaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ASP_NET_ INMN3(hybryda)_PA2_Z1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP_NET__INMN3_hybryda__PA2_Z1.Pages;
+public class PrivacyModel : PageModel
+{
+	private readonly ILogger<PrivacyModel> _logger;
+
+	public PrivacyModel(ILogger<PrivacyModel> logger)
+	{
+		_logger = logger;
+	}
+
+	public void OnGet()
+	{
+	}
+}
+
